76f2880dbe06894497d8af36282cdb07efb4a557
Weidaicheng <wdcdavyc@outlook.com>
2018-04-07 15:38:06 +0800

    教程（2）

--- WebApiA/Controllers/ValuesController.cs
+++ WebApiA/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace WebApiA.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return $"value {id} from WebApiA";
         }
 
         // POST api/values
--- WebApiB/Controllers/ValuesController.cs
+++ WebApiB/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace WebApiB.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return $"value {id} from WebApiB";
         }
 
         // POST api/values
