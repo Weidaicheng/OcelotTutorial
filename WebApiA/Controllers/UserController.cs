de5cad2197def8bf10c5875aa3eeb5cea47dc082
Weidaicheng <wdcdavyc@outlook.com>
2018-04-10 19:22:40 +0800

    教程（3.1）

---
+++ WebApiA/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiA.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public string GetSex(string name)
+        {
+            if (name == "Jonathan")
+                return "Man";
+            return null;
+        }
+
+        [HttpGet]
+        public int? GetAge(string name)
+        {
+            if (name == "Jonathan")
+                return 24;
+            return null;
+        }
+    }
+}
