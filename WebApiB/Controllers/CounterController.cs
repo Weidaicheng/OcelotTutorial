ae2d80791b3c2ebb893c504c6ce9f46ed70dab54
Weidaicheng <wdcdavyc@outlook.com>
2018-04-27 22:22:01 +0800

    教程（3.3）

---
+++ WebApiB/Controllers/CounterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiB.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class CounterController : Controller
+    {
+        private static int _count = 0;
+
+        [HttpGet]
+        public string Count()
+        {
+            return $"Count {++_count} from WebapiB";
+        }
+    }
+}
