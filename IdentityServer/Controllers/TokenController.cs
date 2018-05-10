7877bcbc6c7a911fdc68381b041e562fbdb265f8
Weidaicheng <wdcdavyc@outlook.com>
2018-05-10 21:04:10 +0800

    教程（4）

---
+++ IdentityServer/Controllers/TokenController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace IdentityServer.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Token")]
+    public class TokenController : Controller
+    {
+        public async Task<JObject> Get()
+        {
+            var disco = await DiscoveryClient.GetAsync($"{Request.Scheme}://{Request.Host}");
+            if(disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return null;
+            }
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+            if(tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return null;
+            }
+
+            return tokenResponse.Json;
+        }
+    }
+}
--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -2,18 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using IdentityServer4.AccessTokenValidation;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
 
-namespace OcelotGetway
+namespace IdentityServer
 {
     public class Startup
     {
@@ -29,31 +25,22 @@ namespace OcelotGetway
         {
             services.AddMvc();
 
-            services.AddOcelot(new ConfigurationBuilder()
-                .AddJsonFile("configuration.json")
-                .Build());
-
             services
-                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddIdentityServerAuthentication("TestKey", options =>
-                {
-                    options.Authority = "http://localhost:6000";
-                    options.RequireHttpsMetadata = false;
-                    options.ApiName = "api1";
-                });
+                .AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            await app.UseOcelot();
-
-            app.UseAuthentication();
+            app.UseIdentityServer();
 
             app.UseMvc();
         }
